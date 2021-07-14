b17b0752404a98602097f1beded020dcd5f0417d
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2021-07-14 14:58:26 +0400

    feat: mock string from server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
-const app = require("express")();
-const http = require("http").Server(app);
-const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const port = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+	res.send(`This mock string for Vikentii's client`);
+});
 
-const voicesArr = [];
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+});
 
-app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, '../client/index.html'));
-});
-app.get("/voices", function (req, res) {
-	res.send(voicesArr);
-});
-app.get('/index.js', function (req, res) {
-	res.sendFile((path.join(__dirname, '../client/index.js')));
-});
+
+// const app = require("express")();
+// const http = require("http").Server(app);
+// const path = require('path');
+
+// const port = process.env.PORT || 3000;
+
+// const voicesArr = [];
+
+// app.get("/", function (req, res) {
+// 	res.sendFile(path.join(__dirname, '../client/index.html'));
+// });
+// app.get("/voices", function (req, res) {
+// 	res.send(voicesArr);
+// });
+// app.get('/index.js', function (req, res) {
+// 	res.sendFile((path.join(__dirname, '../client/index.js')));
+// });
