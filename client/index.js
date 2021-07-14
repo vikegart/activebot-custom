37b6100b87ffde5e584503da0468f27d6567468a
Vikentii Ekgart <vikegart@gmail.com>
2021-07-14 13:42:26 +0400

    async get data

--- client/index.js
+++ client/index.js
@@ -1,7 +1,15 @@
 import fakedata from "./fakedata.js"
 
-// const DISPLAY = document.getElementById("display");
-// DISPLAY.innerHTML = fakedata;
+const BODY = document.getElementsByTagName("body")[0];
 
-document.getElementsByTagName("body")[0].innerHTML = fakedata;
-console.log('hello');
+const render = data => BODY.innerHTML = data;
+
+const getData = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => { resolve(fakedata) }, 200);
+    })
+}
+
+setInterval(() => {
+    getData().then(data => render(data));
+}, 500)
