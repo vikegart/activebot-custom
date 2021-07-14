5162bd519b10b89b4846a5999941e3f2967d2913
Vikentii Ekgart <vikegart@gmail.com>
2021-07-14 13:20:54 +0400

    fake client

--- client/index.js
+++ client/index.js
@@ -1,1 +1,7 @@
+import fakedata from "./fakedata.js"
+
+// const DISPLAY = document.getElementById("display");
+// DISPLAY.innerHTML = fakedata;
+
+document.getElementsByTagName("body")[0].innerHTML = fakedata;
 console.log('hello');
