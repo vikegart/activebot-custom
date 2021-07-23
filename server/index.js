e5180e1b299d4224e89487b3dc578678bf5103aa
Vikentii Ekgart <vikegart@gmail.com>
2021-07-23 14:00:26 +0400

    url

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use('/', express.static('client/'));
 
 const port = process.env.PORT || 3000;
-const vgmUrl = process.env.URL || 'https://activebot.ru/c/zJ2JjqjJz';
+const vgmUrl = process.env.URL || 'https://activebot.ru/c/gZp1rDv7z';
 const TIMEOUT = 1000;
 
 let tableData;
